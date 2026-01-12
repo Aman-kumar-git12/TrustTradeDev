e8afc193c10e6e5158b768d4ba9f15e9fb641f17
Aman kumar <rajaman5562@gmail.com>
2026-01-12 22:39:21 +0530

    fixes done

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || (import.meta.env.MODE === 'production' ? 'https://trusttrade-6d81.onrender.com/api' : 'http://localhost:5002/api'),
+    baseURL: `${import.meta.env.VITE_API_URL}`,
     withCredentials: true // Important for cookies
 });
 
